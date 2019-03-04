b9950a4f928dba8ebc5f53a6e12867ce55e54d43
ACMOORE\doravsky <doravsky@acmoore.com>
2019-03-04 17:01:13 -0500

    Merge from meancore ## Remove unused code ## Add ssr with puppeteer

--- server/core/core.server.controller.js
+++ server/core/core.server.controller.js
@@ -1,6 +1,9 @@
 'use strict';
 
-var path = require('path');
+var path = require('path'),
+  config = require(path.resolve('./config/config')),
+  isBot = config.helpers.isBot,
+  ssrService = config.services.ssrService;
 
 /**
  * Get extention from path
@@ -19,16 +22,37 @@ var renderNotFound = function (req, res) {
 };
 exports.renderNotFound = renderNotFound;
 
+// check for webcrawlers and prerender
+// otherwise move on to render index
+exports.prerender = async function (req, res, next) {
+  if (!isBot(req.headers['user-agent'])) {
+    return next();
+  } else {
+
+    if (req.query.prerender) {
+      return next();
+    } else {
+      const {
+        html,
+        ttRenderMs
+      } = await ssrService.ssr(`${req.protocol}://${req.get('host')}${req.url}?prerender=true`);
+      // Add Server-Timing! See https://w3c.github.io/server-timing/.
+      res.set('Server-Timing', `Prerender;dur=${ttRenderMs};desc="Headless render time (ms)"`);
+      return res.status(200).send(html + '<!-- SSR -->'); // Serve prerendered page as response.
+    }
+
+  };
+}
+
 /**
  * Render the main application page
  */
-exports.renderIndex = function (req, res) {
+exports.renderIndex = async function (req, res) {
   var config = req.app.locals.config;
   var ext = getExtention(req.url);
   // TODO: change the way to identify extentions
   var exts = ['html', 'css', 'js', 'pdf', 'jpeg', 'gif', 'png'];
   if (!config.app.defaultPage || ~exts.indexOf(ext)) { // if not in extentions give 404
-    // next();
     renderNotFound(req, res); // We know its not found
   } else {
     var rootDir = path.normalize(config.staticFiles);
