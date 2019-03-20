0d4175ccf34622b1ddfe2b99a87041915d8db8ce
david oravsky <david.oravsky@gmail.com>
2019-03-19 22:35:20 -0400

    Remove old image routes and methods

--- server/cms/admin/admin.server.controller.js
+++ server/cms/admin/admin.server.controller.js
@@ -1,12 +1,8 @@
 'use strict';
 
 var path = require('path'),
-    multer = require('multer'),
-    _ = require('lodash'),
     config = require(path.resolve('./config/config')),
-    fs = require("fs"),
-    slugify = config.helpers.slugify,
-    fileExists = config.helpers.fileExists;
+    fs = require("fs");
 
 exports.checkInstall = function (req, res, next) {
     try {
@@ -22,40 +18,3 @@ exports.checkInstall = function (req, res, next) {
 
     next();
 }
-
-exports.upload = function (req, res, next) {
-    // respond with image url
-    res.status(200).send({
-        "uploaded": true,
-        "url": res.locals.host + '/api/admin/image-uploads/' + req.file.filename
-    });
-}
-
-var getUpload = function () {
-    // file upload config using multer
-    var uploadDir = config.imageUploadRepository;
-
-    var storage = multer.diskStorage({
-        destination: uploadDir,
-        filename: function (req, file, cb) {
-            var fileExtension = path.extname(file.originalname);
-            var fileBase = path.basename(file.originalname, fileExtension);
-            var fileSlug = slugify(fileBase) + fileExtension;
-
-            // ensure file name is unique by adding a counter suffix if the file exists
-            var fileCounter = 0;
-            while (fileExists(path.join(uploadDir, fileSlug))) {
-                fileCounter += 1;
-                fileSlug = slugify(fileBase) + '-' + fileCounter + fileExtension;
-            }
-
-            cb(null, fileSlug);
-        }
-    });
-    var upload = multer({
-        storage: storage
-    });
-
-    return upload;
-}
-exports.getUpload = getUpload;
--- server/cms/admin/admin.server.routes.js
+++ server/cms/admin/admin.server.routes.js
@@ -1,29 +1,11 @@
 'use strict';
 
-var express = require('express'),
-  path = require('path'),
+var path = require('path'),
   config = require(path.resolve('./config/config')),
   userAuth = require(path.resolve('./server/users/users.authorization.server.controller'));
-
-var oneWeekSeconds = 60 * 60 * 24 * 7;
-var oneWeekMilliseconds = oneWeekSeconds * 1000; // 1 week in milliseconds,
 
 module.exports = function (app) {
   var admin = require('./admin.server.controller');
 
   app.use(['/admin'], admin.checkInstall, userAuth.hasAuthorization(['admin'], config.appBase));
-
-  app.use('/api/admin/image-uploads', express.static(path.resolve(config.imageUploadRepository), {
-    maxAge: oneWeekMilliseconds,
-    index: false
-  }));
-
-  app.use('/api/admin/content/images', express.static(path.resolve(config.adminContentRepository + 'images/'), {
-    maxAge: oneWeekMilliseconds,
-    index: false
-  }));
-
-  // use session auth to secure the front end admin files
-  // handle file upload
-  app.route('/api/admin/upload').post(admin.getUpload().single('upload'), admin.upload);
 };
